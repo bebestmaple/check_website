c6d0a508b1b4fb9dcbc80ad8184c2294e4390764
HoyinNgai <hoyinngai@hotmail.com>
2021-08-07 16:24:29 +0800

    fix

---
+++ check_website.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+test -f result.log && rm -f result.log
+echo '' > result.log
+for url in $@
+do
+        #curl抓取网站http状态码
+        code=`curl -o /dev/null --retry 3 --retry-max-time 8 -s -w %{http_code} $url`
+        echo "$code ---> $url">>result.log
+done
+
+#找出非200返回码的站点
+echo List of exception website:
+cat result.log | grep -v 200
+exit 0
