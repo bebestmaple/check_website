fc76131831d0a08499d5048f23a67744cbf0ac1e
bebestmaple <bebestmaple@users.noreply.github.com>
2021-08-09 09:41:58 +0800

    Update check_website.sh

--- check_website.sh
+++ check_website.sh
@@ -6,7 +6,7 @@ for url in $@
 do
         #curl抓取网站http状态码
         code=`curl -o /dev/null --retry 3 --retry-max-time 8 -s -w %{http_code} $url`
-        echo "$code ---> $url">>result.log
+        echo " $code ---> $url " >> result.log
 done
 
 #找出非200返回码的站点
